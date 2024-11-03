bbf62d3510ecc950f7781f378b8e30ca40b3c452
Никита <153456515+Lairont@users.noreply.github.com>
2024-11-04 00:40:48 +0200

    Add files via upload

---
+++ ConsoleApp111/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.вывести в столбик 10 раз слово "Привет";
+            //int count = 1;
+            //while(count<=10)
+            //{
+            //    Console.WriteLine("Привет");
+            //    count++;
+            //}
+            //Console.ReadLine();
+
+            //do
+            //{
+            //    Console.WriteLine("Привет");
+            //    count+=1;
+            //}
+            //while (count<=10);
+            //Console.ReadLine();
+
+            //for(int count=1;count<=10;count++)
+            //{
+            //    Console.WriteLine("Привет");
+            //}
+            //Console.ReadLine();
+
+
+            //2.вывести в столбик первые 10 натуральных чисел;
+            //int count = 0;
+            //while(count<10)
+            //{
+            //    count++;
+            //        Console.WriteLine($"{count}");
+            //}
+            //Console.ReadLine();
+
+            //do
+            //{
+            // count ++;
+            //    Console.WriteLine($"{count}");
+            //}
+            //while (count < 10);
+            //Console.ReadLine();
+
+            //for (int count=1;count<=10;count++)
+            //{
+            //    Console.WriteLine($"{count}");
+            //}
+            //Console.ReadLine();
+
+
+            //3.вывести в столбик первые 5 натуральных чисел в обратном порядке;
+            //int count = 6;
+            //while (count>1)
+            //{
+            //    count--;
+            //    Console.WriteLine($"{count}");
+            //}
+            //Console.ReadLine();
+
+            //do
+            //{
+            //    count--;
+            //    Console.WriteLine($"{count}");
+            //}
+            //while (count>1);
+            //Console.ReadLine();
+
+            //for (int count=5;count>0;count--)
+            //{
+            //    Console.WriteLine($"{count}");
+            //}
+            //Console.ReadLine();
+
+
+            //4.вывести на экран целые числа из интервала от 1 до n;
+            //Console.Write("n= ");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 0;
+            //while (count<n)
+            //{
+            //    count++;
+            //    Console.WriteLine($"{count}");
+            //}
+            //Console.ReadLine();
+
+            //do
+            //{
+            //    count++;
+            //    Console.WriteLine($"{count}");
+            //}
+            //while (count<n);
+            //Console.ReadLine();
+
+            //for (int count = 1; count<=n; count++)
+            //{
+            //    Console.WriteLine($"{count}");
+            //}
+            //Console.ReadLine();
+
+
+            //5.запросить у пользователя 5 слов, после чего вывести вначале дважды каждое нечетное слово и затем трижды каждое четное слово, между выводимыми словами вставить разделитель в виде трех символьных пробелов(“   ”).
+            //string[] words = new string[5];
+
+            //Console.WriteLine("Введите 5 слов: ");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"word {i + 1}: ");
+            //    words[i] = Console.ReadLine();
+            //}
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    if ((i + 1) % 2 != 0) 
+            //    {
+            //        Console.Write(words[i] + "   " + words[i] + "   ");
+            //    }
+            //}
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+           
+            //    if ((i + 1) % 2 == 0) 
+            //    {
+            //        Console.Write(words[i] + "   " + words[i] + "   " + words[i] + "   ");
+            //    }
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
